b816661921d86ef829939d097af4f42901d59f3a
Jack Lam <jacklam718@gmail.com>
2017-02-21 01:20:59 +0800

    update AnimatedOverlay.js
    1. add callback for animation finished
    2. start animation on component did mount
    3. add initValue props for component init to

--- src/AnimatedOverlay.js
+++ src/AnimatedOverlay.js
@@ -7,46 +7,68 @@ const { width: WIDTH, height: HEIGHT } = Dimensions.get('window');
 
 class AnimatedOverlay extends Component {
   props: {
-    onPress?: Function;
-    backgroundColor: string;
-    opacity: number;
-    animationDuration: number;
-    overlayShow: boolean;
-    pointerEvents: string;
+    onPress?: () => void;
+    backgroundColor?: string;
+    opacity?: number;
+    duration?: number;
+    overlayShow?: boolean;
+    pointerEvents?: string;
+    initValue?: number;
+    onAnimationFinished?: (value: number) => void;
   }
 
   static defaultProps = {
+    onPress: () => {},
+    pointerEvents: null,
     backgroundColor: '#000',
     opacity: 0.5,
-    animationDuration: 200,
+    duration: 200,
     overlayShow: false,
+    initValue: 0,
+    onAnimationFinished: () => {},
   }
 
   constructor(props) {
     super(props);
-    const toValue = props.overlayShow ? props.opacity : 0;
+
     this.state = {
-      opacity: new Animated.Value(toValue),
+      opacity: new Animated.Value(props.initValue),
+      overlayShow: props.overlayShow,
     };
+  }
+
+  componentDidMount() {
+    this.doAnimation();
   }
 
   componentWillReceiveProps(nextProps) {
     if (this.props.overlayShow !== nextProps.overlayShow) {
-      const toValue = nextProps.overlayShow ? nextProps.opacity : 0;
-      Animated.timing(this.state.opacity, {
-        toValue,
-        duration: this.props.animationDuration,
-      }).start();
+      this.setState({ overlayShow: nextProps.overlayShow });
     }
+  }
+
+  componentDidUpdate() {
+    this.doAnimation();
+  }
+
+  doAnimation() {
+    const { overlayShow, opacity, duration, onAnimationFinished } = this.props;
+    const toValue = overlayShow ? opacity : 0;
+    Animated.timing(this.state.opacity, {
+      toValue,
+      duration,
+    }).start(() => {
+      onAnimationFinished(toValue);
+    });
   }
 
   render() {
     let { pointerEvents } = this.props;
-    const { onPress, overlayShow } = this.props;
+    const { onPress } = this.props;
     const backgroundColor = { backgroundColor: this.props.backgroundColor };
     const opacity = { opacity: this.state.opacity };
 
-    if (!pointerEvents) pointerEvents = overlayShow ? 'auto' : 'none';
+    if (!pointerEvents) pointerEvents = this.state.overlayShow ? 'auto' : 'none';
 
     return (
       <Animated.View
